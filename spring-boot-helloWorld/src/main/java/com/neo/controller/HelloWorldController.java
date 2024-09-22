44fdd921840203a40caaea6675b1e3eeb47b2181
junji lai <33259190+laijunji@users.noreply.github.com>
2024-09-22 18:15:52 +0800

    fix

--- spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.neo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping("/hello")
 @RestController
 public class HelloWorldController {
-	
-    @RequestMapping("/hello")
+    @GetMapping
     public String index() {
         return "Hello World";
     }
